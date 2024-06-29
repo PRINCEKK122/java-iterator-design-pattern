2726f9ae144cda14b8552d739e31fc5ce58c63e9
PRINCEKK122 <awuahkarikari12@gmail.com>
2024-06-29 14:43:23 +0000

    implement menu item

---
+++ src/MenuItem.java
@@ -0,0 +1,29 @@
+public class MenuItem {
+    private final String name;
+    private final String description;
+    private final boolean isVegetarian;
+    private final double price;
+
+    public MenuItem(String name, String description, boolean isVegetarian, double price) {
+        this.name = name;
+        this.description = description;
+        this.isVegetarian = isVegetarian;
+        this.price = price;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public boolean isVegetarian() {
+        return isVegetarian;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+}
